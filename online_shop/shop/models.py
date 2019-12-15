ad531e7c7f1a9cce355e9021a346f8e1bd7a9d99
Calvados <flamaster0807@gmail.com>
2019-12-15 11:09:33 +0200

    created Category and Product models

--- online_shop/shop/models.py
+++ online_shop/shop/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', verbose_name="Category", on_delete=models.PROTECT)
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Name")
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name="Product image")
+    description = models.TextField(blank=True, verbose_name="description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    stock = models.PositiveIntegerField(verbose_name="In stock")
+    available = models.BooleanField(default=True, verbose_name="Available")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        abstract =True
+        index_together = [
+            ['id', 'slug']
+        ]
+
+    def __str__(self):
+        return self.name
